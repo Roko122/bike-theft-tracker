b33b56cc137f6cf7c046bcaafd0b06e1f5185178
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-29 17:44:52 +0200

    BTT-32 Fix User table name from user to users

--- backend/src/main/java/com/rkrs/bikethefttracker/domain/User.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/domain/User.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor @Builder
 public class User {
