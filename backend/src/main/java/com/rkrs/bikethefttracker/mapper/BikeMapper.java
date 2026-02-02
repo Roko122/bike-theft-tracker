c82dd6457804897a59f2073ab55662f490814b31
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-02 15:49:13 +0200

    BTT-36 Add toBikeResponse and toBike methods into BikeMapper

--- backend/src/main/java/com/rkrs/bikethefttracker/mapper/BikeMapper.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/mapper/BikeMapper.java
@@ -1,7 +1,49 @@
 package com.rkrs.bikethefttracker.mapper;
 
+import com.rkrs.bikethefttracker.domain.Bike;
+import com.rkrs.bikethefttracker.domain.TheftReport;
+import com.rkrs.bikethefttracker.domain.User;
+import com.rkrs.bikethefttracker.dto.BikeInfo;
+import com.rkrs.bikethefttracker.dto.BikeResponse;
+import com.rkrs.bikethefttracker.dto.UserResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BikeMapper {
+
+    private final UserMapper userMapper;
+
+    public BikeMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    public BikeResponse toBikeResponse(TheftReport theftReport) {
+        Bike bike = theftReport.getBike();
+        UserResponse owner = userMapper.toUserResponse(theftReport);
+
+        return new BikeResponse(
+                bike.getId(),
+                bike.getBrand(),
+                bike.getModel(),
+                bike.getType(),
+                bike.getColor(),
+                bike.getSerialNumber(),
+                bike.getDescription(),
+                owner
+        );
+    }
+
+    public Bike toBike(BikeInfo bikeInfo) {
+        User user = userMapper.toUser(bikeInfo.owner());
+
+        return Bike.builder()
+                .brand(bikeInfo.brand())
+                .model(bikeInfo.model())
+                .type(bikeInfo.type())
+                .color(bikeInfo.color())
+                .serialNumber(bikeInfo.serialNumber())
+                .description(bikeInfo.description())
+                .owner(user)
+                .build();
+    }
 }
