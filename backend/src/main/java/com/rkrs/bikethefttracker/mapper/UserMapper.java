8e7942d940ca30ea1ba531e437d4e479fc1145f0
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-02 15:28:48 +0200

    BTT-36 Add toUserResponse and toUser methods for UserMapper

--- backend/src/main/java/com/rkrs/bikethefttracker/mapper/UserMapper.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/mapper/UserMapper.java
@@ -1,7 +1,26 @@
 package com.rkrs.bikethefttracker.mapper;
 
+import com.rkrs.bikethefttracker.domain.TheftReport;
+import com.rkrs.bikethefttracker.domain.User;
+import com.rkrs.bikethefttracker.dto.OwnerInfo;
+import com.rkrs.bikethefttracker.dto.UserResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
+
+    public UserResponse toUserResponse(TheftReport theftReport) {
+        User user = theftReport.getBike().getOwner();
+        return new UserResponse(
+                user.getId(),
+                user.getUsername()
+        );
+    }
+
+    public User toUser(OwnerInfo ownerInfo) {
+        return User.builder()
+                .email(ownerInfo.email())
+                .username(ownerInfo.username())
+                .build();
+    }
 }
