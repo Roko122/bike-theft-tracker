e5557ffd9e8b47133d44b966b9a0e489470a8b65
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-02 13:48:42 +0200

    BTT-36 Add toPoint and toGeoPoint methods for GeoPointMapper

--- backend/src/main/java/com/rkrs/bikethefttracker/mapper/GeoPointMapper.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/mapper/GeoPointMapper.java
@@ -1,7 +1,31 @@
 package com.rkrs.bikethefttracker.mapper;
 
+import com.rkrs.bikethefttracker.dto.GeoPoint;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GeoPointMapper {
+
+    private final GeometryFactory geometryFactory;
+
+    public GeoPointMapper() {
+        this.geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
+    }
+
+    public Point toPoint(GeoPoint geoPoint) {
+        Coordinate coordinates = new Coordinate(geoPoint.longitude(), geoPoint.latitude());
+
+        return geometryFactory.createPoint(coordinates);
+    }
+
+    public GeoPoint toGeoPoint(Point point) {
+        double lon = point.getX();
+        double lat = point.getY();
+
+        return new GeoPoint(lon, lat);
+    }
 }
