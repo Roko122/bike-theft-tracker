f8ac548c1908754e42066fb2c9d1e465ea2be7d5
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-02 13:03:32 +0200

    BTT-34 Add needed fields for BikeResponse

--- backend/src/main/java/com/rkrs/bikethefttracker/dto/BikeResponse.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/BikeResponse.java
@@ -1,4 +1,15 @@
 package com.rkrs.bikethefttracker.dto;
 
-public record BikeResponse() {
+import java.util.UUID;
+
+public record BikeResponse(
+        UUID id,
+        String brand,
+        String model,
+        String type,
+        String color,
+        String serialNumber,
+        String description,
+        UserResponse owner
+) {
 }
