3445278384baad8ee1c59dfeeee4ce0d1e126004
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-02 13:08:23 +0200

    BTT-34 Add needed fields for BikeInfo

--- backend/src/main/java/com/rkrs/bikethefttracker/dto/BikeInfo.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/BikeInfo.java
@@ -1,4 +1,12 @@
 package com.rkrs.bikethefttracker.dto;
 
-public record BikeInfo() {
+public record BikeInfo(
+        String brand,
+        String model,
+        String type,
+        String color,
+        String serialNumber,
+        String description,
+        OwnerInfo owner
+) {
 }
