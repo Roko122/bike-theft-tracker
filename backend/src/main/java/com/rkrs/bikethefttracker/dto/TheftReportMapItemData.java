9a72a8c1d13cf7ed6e757aefd0888ac23a085a2f
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-03 16:32:11 +0200

    BTT-21 Add TheftReportMapItemData DTO for database query

---
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/TheftReportMapItemData.java
@@ -0,0 +1,19 @@
+package com.rkrs.bikethefttracker.dto;
+
+import com.rkrs.bikethefttracker.domain.Status;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TheftReportMapItemData (
+        UUID id,
+        String brand,
+        String model,
+        String type,
+        String color,
+        Status status,
+        Point location,
+        LocalDateTime theftTime
+) {
+}
