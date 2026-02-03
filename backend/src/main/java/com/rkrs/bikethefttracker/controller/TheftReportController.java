e772224eb411a7163dc6636ea3e1e91d80de5cec
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-03 15:29:15 +0200

    BTT-21 Add skeleton of TheftReportController

---
+++ backend/src/main/java/com/rkrs/bikethefttracker/controller/TheftReportController.java
@@ -0,0 +1,10 @@
+package com.rkrs.bikethefttracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/theft-reports")
+public class TheftReportController {
+
+}
