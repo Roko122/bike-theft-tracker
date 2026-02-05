9d96ce860e6b8bf1cc588c7f245dda05a42e9ab6
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-05 18:07:32 +0200

    BTT-61 Add query to find TheftReport by id

--- backend/src/main/java/com/rkrs/bikethefttracker/repository/TheftReportRepository.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/repository/TheftReportRepository.java
@@ -2,11 +2,13 @@ package com.rkrs.bikethefttracker.repository;
 
 import com.rkrs.bikethefttracker.domain.TheftReport;
 import com.rkrs.bikethefttracker.dto.TheftReportMapItemData;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -27,4 +29,7 @@ public interface TheftReportRepository extends JpaRepository<TheftReport, UUID> 
             JOIN tp.bike b
     """)
     List<TheftReportMapItemData> findAllTheftReportMapItems();
+
+    @EntityGraph(attributePaths = {"bike", "bike.user"})
+    Optional<TheftReport> findById(UUID id);
 }
