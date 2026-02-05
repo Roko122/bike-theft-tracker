a9c2b1a6d2c1c6adbcbc5234cc8e26dd38ed5263
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-05 13:02:48 +0200

    BTT-23 Fix typo in TheftReportResponse

--- backend/src/main/java/com/rkrs/bikethefttracker/dto/TheftReportResponse.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/TheftReportResponse.java
@@ -12,7 +12,7 @@ public record TheftReportResponse(
     String theftAddress,
     GeoPoint location,
     Status status,
-    LocalDateTime createAt,
+    LocalDateTime createdAt,
     BikeResponse bike
 ) {
 }
