e208a306cd7ce87a926132a3917e8e38303e4db4
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-05 13:04:11 +0200

    BTT-23 Add method to save a User

--- backend/src/main/java/com/rkrs/bikethefttracker/service/UserService.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/service/UserService.java
@@ -1,7 +1,23 @@
 package com.rkrs.bikethefttracker.service;
 
+import com.rkrs.bikethefttracker.domain.User;
+import com.rkrs.bikethefttracker.dto.UserInfo;
+import com.rkrs.bikethefttracker.mapper.UserMapper;
+import com.rkrs.bikethefttracker.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    public UserService(UserMapper userMapper, UserRepository userRepository) {
+        this.userMapper = userMapper;
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(UserInfo owner) {
+        User userToCreate = userMapper.toUser(owner);
+        return userRepository.save(userToCreate);
+    }
 }
