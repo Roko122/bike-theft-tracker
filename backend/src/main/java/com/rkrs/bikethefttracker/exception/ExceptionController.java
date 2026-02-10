ffa4db9f1f48ed1569927029927ba7cf70f5f432
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-10 16:37:01 +0200

    BTT-35 Add logging

--- backend/src/main/java/com/rkrs/bikethefttracker/exception/ExceptionController.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/exception/ExceptionController.java
@@ -1,18 +1,21 @@
 package com.rkrs.bikethefttracker.exception;
 
 import com.rkrs.bikethefttracker.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ExceptionController {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException() {
+    public ErrorResponse handleException(Exception ex) {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        log.error("Exception handled:", ex);
 
         return new ErrorResponse(
                 httpStatus.getReasonPhrase(),
@@ -25,6 +28,7 @@ public class ExceptionController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundException ex) {
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        log.warn(ex.getMessage());
 
         return new ErrorResponse(
                 httpStatus.getReasonPhrase(),
