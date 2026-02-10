50299e2123fd84819064dca828530eff46eec3bf
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-10 16:18:31 +0200

    BTT-35 Add exception handler for unexpected exceptions (Exception)

--- backend/src/main/java/com/rkrs/bikethefttracker/exception/ExceptionController.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/exception/ExceptionController.java
@@ -1,8 +1,21 @@
 package com.rkrs.bikethefttracker.exception;
 
+import com.rkrs.bikethefttracker.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
 
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException() {
+        return new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "An unexpected error occurred"
+        );
+    }
 }
