9fea9768bae608de4635cf2de8c1e19acc2166f7
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-16 10:13:25 +0200

    BTT-57 Add validation to BikeInfo

--- backend/src/main/java/com/rkrs/bikethefttracker/dto/BikeInfo.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/BikeInfo.java
@@ -1,12 +1,35 @@
 package com.rkrs.bikethefttracker.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record BikeInfo(
+
+        @NotBlank(message = "Brand must not be empty.")
+        @Size(max = 30, message = "Brand must not exceed ${max} characters.")
         String brand,
+
+        @NotBlank(message = "Model must not be empty.")
+        @Size(max = 30, message = "Model must not exceed ${max} characters.")
         String model,
+
+        @NotBlank(message = "Type must not be empty.")
+        @Size(max = 30, message = "Type must not exceed ${max} characters.")
         String type,
+
+        @NotBlank(message = "Color must not be empty.")
+        @Size(max = 30, message = "Color must not exceed ${max} characters.")
         String color,
+
+        @Size(max = 30, message = "Serial number must not exceed ${max} characters.")
         String serialNumber,
+
+        @NotBlank(message = "Description must not be empty.")
+        @Size(max = 300, message = "Description must not exceed ${max} characters.")
         String description,
+
+        @Valid
         UserInfo user
 ) {
 }
