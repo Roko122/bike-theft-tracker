b95f34fdf4b44f4fb9e0d856927e991c6e25e6e1
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-16 10:35:58 +0200

    BTT-57 Remove unnecessary characters from validation error messages

--- backend/src/main/java/com/rkrs/bikethefttracker/dto/CreateTheftReportRequest.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/CreateTheftReportRequest.java
@@ -11,14 +11,14 @@ import java.time.LocalDateTime;
 public record CreateTheftReportRequest(
 
         @NotBlank(message = "Description must not be empty.")
-        @Size(max = 300, message = "Description must not exceed ${max} characters.")
+        @Size(max = 300, message = "Description must not exceed {max} characters.")
         String description,
 
         @NotNull(message = "Theft time is required.")
         @Past(message = "Theft time must be in the past.")
         LocalDateTime theftTime,
 
-        @Size(max = 150, message = "Theft address must not exceed ${max} characters.")
+        @Size(max = 150, message = "Theft address must not exceed {max} characters.")
         String theftAddress,
 
         @Valid
--- backend/src/main/java/com/rkrs/bikethefttracker/dto/CreateUserRequest.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/CreateUserRequest.java
@@ -7,11 +7,11 @@ import jakarta.validation.constraints.Size;
 public record CreateUserRequest(
 
         @NotBlank(message = "Username must not be empty.")
-        @Size(min = 3, max = 20, message = "Username must be between ${min}-${max} characters.")
+        @Size(min = 3, max = 20, message = "Username must be between {min}-{max} characters.")
         String username,
 
         @NotBlank(message = "Username must not be empty.")
-        @Size(min = 6, max = 254, message = "Email must be between ${min}-${max} characters.")
+        @Size(min = 6, max = 254, message = "Email must be between {min}-{max} characters.")
         @Email(message = "Email must be valid.")
         String email
 ) {
