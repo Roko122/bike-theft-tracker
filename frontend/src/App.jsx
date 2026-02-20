e2b73bb638032b7f81ed95bef457199942c7bffa
Pearl <helmi.rauhala1@gmail.com>
2026-02-20 12:45:48 +0100

    BTT-42 og

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,37 @@
 import MapPage from "./features/map/MapPage.jsx";
-
-import MapPage from "./features/map/MapPage.jsx";
+import { useState } from "react";
 
 export default function App() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="app-shell">
       <header className="header">
-        <button className="menu-btn">☰</button>
+        <button
+          className="menu-btn"
+          onClick={() => setOpen((v) => !v)}
+        >
+          {open ? "✕" : "☰"}
+        </button>
+
+
         <div className="title">
           <strong> Bike Tracker</strong>
         </div>
         <div className="speacer">
 
         </div>
+        {open && (
+  <div className="map-menu" onClick={() => setOpen(false)}>
+    <button onClick={(e) => e.stopPropagation()}>
+      heloo world
+    </button>
+
+    <button onClick={(e) => e.stopPropagation()}>
+      jotain tärkeää
+    </button>
+  </div>
+)}
+
       </header>
       <main className="main">
         <MapPage />
