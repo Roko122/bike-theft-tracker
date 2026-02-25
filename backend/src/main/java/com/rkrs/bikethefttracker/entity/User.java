7664a4a5426ada7abb46b1c295c7086ec0838261
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-25 14:34:00 +0200

    BTT-70 Add password column to User entity

--- backend/src/main/java/com/rkrs/bikethefttracker/entity/User.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/entity/User.java
@@ -21,6 +21,9 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username;
 
+    @Column(nullable = false)
+    private String password;
+
     @Column(unique = true, nullable = false)
     private String email;
 
