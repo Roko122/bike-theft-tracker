7eab3e13a811e53a5ebce4a0760077b4a8a50366
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-26 16:51:50 +0200

    BTT-66 Update CustomUserDetails to get authorities from User-entity

--- backend/src/main/java/com/rkrs/bikethefttracker/security/CustomUserDetails.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/security/CustomUserDetails.java
@@ -1,12 +1,11 @@
 package com.rkrs.bikethefttracker.security;
 
 import com.rkrs.bikethefttracker.entity.User;
-import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 public class CustomUserDetails implements UserDetails {
 
@@ -18,11 +17,13 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .toList();
     }
 
     @Override
-    public @Nullable String getPassword() {
+    public String getPassword() {
         return user.getPassword();
     }
 
