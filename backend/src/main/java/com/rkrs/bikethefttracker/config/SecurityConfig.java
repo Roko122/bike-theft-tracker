a98bd8f923f91379328966dbf654c4694070bc9f
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-26 13:22:21 +0200

    BTT-66 Add DelegatingPasswordEncoder (defaulting to bcrypt)

--- backend/src/main/java/com/rkrs/bikethefttracker/config/SecurityConfig.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -46,5 +48,10 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
 
         return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
