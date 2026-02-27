66b39aae73d57bf04e69e234c263f14dd7bfce35
verarantakoski <verara@uef.fi>
2026-02-27 12:22:09 +0200

    BTT-43 lisätty eslint

--- frontend/src/features/map/ui/MapControls.jsx
+++ frontend/src/features/map/ui/MapControls.jsx
@@ -1,31 +1,30 @@
-import { useState } from "react";
-import { LocateFixed } from "lucide-react";
+import { LocateFixed } from 'lucide-react';
 
 export default function MapControls({ onCenterToUser }) {
-
   const stop = (e) => {
     e.preventDefault();
     e.stopPropagation();
   };
   return (
-    <div className="map-controls"onClick={stop} onMouseDown={stop} onDoubleClick={stop}>
-    <button
-    className="map-control-btn"
+    <div
+      className="map-controls"
+      onClick={stop}
+      onMouseDown={stop}
+      onDoubleClick={stop}
+    >
+      <button
+        className="map-control-btn"
         onClick={(e) => {
-           e.preventDefault();
-           e.stopPropagation();
-           onCenterToUser();
-  }}
+          e.preventDefault();
+          e.stopPropagation();
+          onCenterToUser();
+        }}
         type="button"
         title="Keskitä sijaintiin"
         aria-label="Keskitä sijaintiin"
-
-        
       >
-      <LocateFixed size={22}/> 
+        <LocateFixed size={22} />
       </button>
-
-     
     </div>
   );
 }
