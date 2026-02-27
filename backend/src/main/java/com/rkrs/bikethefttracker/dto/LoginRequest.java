59f8b608fe4b4319c307bb8c131d3cfcd08a3830
rolfk <172577720+Roko122@users.noreply.github.com>
2026-02-27 16:58:36 +0200

    BTT-91 Add validation to LoginRequest

--- backend/src/main/java/com/rkrs/bikethefttracker/dto/LoginRequest.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/LoginRequest.java
@@ -1,7 +1,16 @@
 package com.rkrs.bikethefttracker.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record LoginRequest(
+
+        @NotBlank(message = "Username must not be empty.")
+        @Size(min = 3, max = 20, message = "Username must be between {min}-{max} characters.")
         String username,
+
+        @NotBlank(message = "Password must not be empty.")
+        @Size(min = 8, max = 64, message = "Password must be between {min}-{max} characters.")
         String password
 ) {
 }
