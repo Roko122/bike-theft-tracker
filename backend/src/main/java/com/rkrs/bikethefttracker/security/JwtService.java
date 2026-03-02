26889762790095bdfc87e38d6a75a11d84763042
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-02 18:47:32 +0200

    BTT-67 Add methods to generate JWT tokens and to check validity of token

--- backend/src/main/java/com/rkrs/bikethefttracker/security/JwtService.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/security/JwtService.java
@@ -1,12 +1,16 @@
 package com.rkrs.bikethefttracker.security;
 
 import com.rkrs.bikethefttracker.properties.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Service
 @EnableConfigurationProperties(JwtProperties.class)
@@ -18,24 +22,36 @@ public class JwtService {
         this.jwtProperties = jwtProperties;
     }
 
-    public String generateAccessToken() {
-        return null;
+    public String generateAccessToken(UserDetails userDetails) {
+        return buildToken(userDetails, jwtProperties.accessTokenExpirationTime());
     }
 
-    public String generateRefreshToken() {
-        return null;
+    public String generateRefreshToken(UserDetails userDetails) {
+        return buildToken(userDetails, jwtProperties.refreshTokenExpirationTime());
     }
 
-    public boolean isTokenValid() {
-        return false;
+    public boolean isTokenValid(String token) {
+        Claims claims = this.extractAllClaims(token); //checks signature
+        Date expiryTime = claims.getExpiration();
+
+        return expiryTime.after(new Date());
     }
 
-    public boolean isRefreshTokenValid() {
-        return false;
+    private String buildToken(UserDetails userDetails, Long expirationTime) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSigningKey())
+                .compact();
     }
 
-    public String extractUsername() {
-        return null;
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     private SecretKey getSigningKey() {
