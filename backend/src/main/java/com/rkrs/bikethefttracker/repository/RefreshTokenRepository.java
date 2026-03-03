9c3775381ed26fdc339a144f8155fe9bc4087115
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-03 12:13:25 +0200

    BTT-67 Add RefreshTokenRepository and RefreshTokenService

---
+++ backend/src/main/java/com/rkrs/bikethefttracker/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.rkrs.bikethefttracker.repository;
+
+import com.rkrs.bikethefttracker.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+}
---
+++ backend/src/main/java/com/rkrs/bikethefttracker/security/RefreshTokenService.java
@@ -0,0 +1,8 @@
+package com.rkrs.bikethefttracker.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RefreshTokenService {
+
+}
