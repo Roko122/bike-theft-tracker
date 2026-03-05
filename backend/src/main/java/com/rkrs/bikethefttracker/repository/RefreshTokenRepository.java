9cd774539ea1237dbc008458363a38247af85b6c
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-05 15:09:18 +0200

    BTT-67 Update findByJwtId method to fetch join User

--- backend/src/main/java/com/rkrs/bikethefttracker/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/repository/RefreshTokenRepository.java
@@ -2,6 +2,7 @@ package com.rkrs.bikethefttracker.repository;
 
 import com.rkrs.bikethefttracker.entity.RefreshToken;
 import com.rkrs.bikethefttracker.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,8 @@ import java.util.UUID;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    @EntityGraph(attributePaths = {"user"})
     Optional<RefreshToken> findByJwtId(UUID jwtId);
 
     void deleteAllByJwtId(UUID jwtId);
