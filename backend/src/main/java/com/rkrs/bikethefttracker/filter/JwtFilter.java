00f0284d1b6bc68130502190c403f7911d84216b
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-05 17:58:44 +0200

    BTT-67 Add basic filter function

--- backend/src/main/java/com/rkrs/bikethefttracker/filter/JwtFilter.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/filter/JwtFilter.java
@@ -1,9 +1,16 @@
 package com.rkrs.bikethefttracker.filter;
 
+import com.rkrs.bikethefttracker.security.JwtService;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +18,38 @@ import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        if (request.getServletPath().equals("/api/v1/auth/refresh")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //TODO:
+        Claims accessToken = null; //jwtService.parseToken(request.);
+        if (accessToken != null && jwtService.isTokenValid(accessToken)) {
+            setAuthentication(accessToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void setAuthentication(Claims accessToken) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(accessToken.getSubject());
+        Authentication auth = UsernamePasswordAuthenticationToken.authenticated(
+                userDetails,
+                null,
+                userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
     }
 }
