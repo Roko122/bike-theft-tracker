cbd4e064454f29fe1b267bcdb77281ee63d964a4
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-06 10:43:12 +0200

    BTT-67 Add method to get access token from cookies, surround jwt handling in try catch block

--- backend/src/main/java/com/rkrs/bikethefttracker/filter/JwtFilter.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/filter/JwtFilter.java
@@ -1,11 +1,15 @@
 package com.rkrs.bikethefttracker.filter;
 
+import com.rkrs.bikethefttracker.properties.JwtProperties;
 import com.rkrs.bikethefttracker.security.JwtService;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,13 +21,17 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@EnableConfigurationProperties(JwtProperties.class)
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final JwtProperties jwtProperties;
 
-    public JwtFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+    public JwtFilter(JwtService jwtService, UserDetailsService userDetailsService, JwtProperties jwtProperties) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+        this.jwtProperties = jwtProperties;
     }
 
     @Override
@@ -34,17 +42,40 @@ public class JwtFilter extends OncePerRequestFilter {
             return;
         }
 
-        //TODO:
-        Claims accessToken = null; //jwtService.parseToken(request.);
-        if (accessToken != null && jwtService.isTokenValid(accessToken)) {
-            setAuthentication(accessToken);
+        try {
+            String accessToken = getAccessTokenFromCookies(request);
+            Claims accessTokenClaims = jwtService.parseToken(accessToken);
+            if (accessTokenClaims != null && jwtService.isTokenValid(accessTokenClaims)) {
+                setAuthentication(accessTokenClaims);
+            }
+        } catch (Exception e) {
+            log.warn("Received invalid auth token from IP {} on {}",
+                    request.getRemoteAddr(),
+                    request.getRequestURI());
         }
 
         filterChain.doFilter(request, response);
     }
 
-    private void setAuthentication(Claims accessToken) {
-        UserDetails userDetails = userDetailsService.loadUserByUsername(accessToken.getSubject());
+    private String getAccessTokenFromCookies(HttpServletRequest request) {
+        String cookieName = jwtProperties.accessTokenCookieName();
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies == null) {
+            return null;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(cookieName)) {
+                return cookie.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    private void setAuthentication(Claims accessTokenClaims) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(accessTokenClaims.getSubject());
         Authentication auth = UsernamePasswordAuthenticationToken.authenticated(
                 userDetails,
                 null,
