dc0597d8f3e429b2b2ccd35f12f158a6ce1ccdc7
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-09 14:00:35 +0200

    Revert "BTT-68 Remove expiryTime from JwtToken"
    This reverts commit afc57469dd283b7869d1be545f18a70e8b0665b0.

--- backend/src/main/java/com/rkrs/bikethefttracker/dto/JwtToken.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/dto/JwtToken.java
@@ -1,9 +1,11 @@
 package com.rkrs.bikethefttracker.dto;
 
+import java.util.Date;
 import java.util.UUID;
 
 public record JwtToken(
         String token,
+        Date expiryTime,
         UUID jwtId
 ) {
 }
