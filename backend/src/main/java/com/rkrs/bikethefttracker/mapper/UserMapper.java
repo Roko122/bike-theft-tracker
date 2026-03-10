94ffef5b7b14add3111cb1aae7e7ea86fbdf9273
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-10 13:24:58 +0200

    BTT-68 Remove unnecessary mapper method

--- backend/src/main/java/com/rkrs/bikethefttracker/mapper/UserMapper.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/mapper/UserMapper.java
@@ -1,8 +1,7 @@
 package com.rkrs.bikethefttracker.mapper;
 
-import com.rkrs.bikethefttracker.entity.User;
-import com.rkrs.bikethefttracker.dto.CreateUserRequest;
 import com.rkrs.bikethefttracker.dto.UserResponse;
+import com.rkrs.bikethefttracker.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,12 +12,5 @@ public class UserMapper {
                 user.getId(),
                 user.getUsername()
         );
-    }
-
-    public User toUser(CreateUserRequest createUserRequest) {
-        return User.builder()
-                .email(createUserRequest.email())
-                .username(createUserRequest.username())
-                .build();
     }
 }
