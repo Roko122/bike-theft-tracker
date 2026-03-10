51b8bab795040bb76e165befb833a317fe4adda5
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-10 13:22:52 +0200

    BTT-68 Update createTheftReport endpoint to include User from Authentication

--- backend/src/main/java/com/rkrs/bikethefttracker/controller/TheftReportController.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/controller/TheftReportController.java
@@ -3,10 +3,13 @@ package com.rkrs.bikethefttracker.controller;
 import com.rkrs.bikethefttracker.dto.CreateTheftReportRequest;
 import com.rkrs.bikethefttracker.dto.TheftReportMapItemResponse;
 import com.rkrs.bikethefttracker.dto.TheftReportResponse;
+import com.rkrs.bikethefttracker.entity.User;
+import com.rkrs.bikethefttracker.security.CustomUserDetails;
 import com.rkrs.bikethefttracker.service.TheftReportService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -51,9 +54,11 @@ public class TheftReportController {
 
     @PostMapping
     public ResponseEntity<TheftReportResponse> createTheftReport(
-            @Valid @RequestBody CreateTheftReportRequest createTheftReportRequest) {
+            @Valid @RequestBody CreateTheftReportRequest createTheftReportRequest,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
 
-        TheftReportResponse createdTheftReport = theftReportService.createTheftReport(createTheftReportRequest);
+        User user = customUserDetails.getUserEntity();
+        TheftReportResponse createdTheftReport = theftReportService.createTheftReport(createTheftReportRequest, user);
 
         return new ResponseEntity<>(createdTheftReport, HttpStatus.CREATED);
     }
