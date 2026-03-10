f9a79c0709933bfb56042d34e17d75f657af090b
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-10 13:21:27 +0200

    BTT-68 Remove unique constraint from User column to allow user's to add multiple theft reports

--- backend/src/main/java/com/rkrs/bikethefttracker/entity/Bike.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/entity/Bike.java
@@ -36,7 +36,7 @@ public class Bike {
     private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(name = "created_at")
