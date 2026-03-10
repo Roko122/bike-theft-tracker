ea8bcd1a1bbf8e39b2dd93fb0065696e881dbb55
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-10 15:56:56 +0200

    BTT-68 Add a method to get UserDetailsResponse from CustomUserDetails

--- backend/src/main/java/com/rkrs/bikethefttracker/mapper/UserMapper.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package com.rkrs.bikethefttracker.mapper;
 
+import com.rkrs.bikethefttracker.dto.UserDetailsResponse;
 import com.rkrs.bikethefttracker.dto.UserResponse;
 import com.rkrs.bikethefttracker.entity.User;
+import com.rkrs.bikethefttracker.security.CustomUserDetails;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +14,14 @@ public class UserMapper {
         return new UserResponse(
                 user.getId(),
                 user.getUsername()
+        );
+    }
+
+    public UserDetailsResponse toUserDetailsResponse(CustomUserDetails user) {
+        return new UserDetailsResponse(
+                user.getUserEntity().getId(),
+                user.getUsername(),
+                user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList()
         );
     }
 }
