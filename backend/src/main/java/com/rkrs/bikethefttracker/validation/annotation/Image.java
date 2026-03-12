061ae392858570d69ab34853a16dbdc6fc538585
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-12 16:51:45 +0200

    BTT-73 Add Image validation

---
+++ backend/src/main/java/com/rkrs/bikethefttracker/validation/annotation/Image.java
@@ -0,0 +1,29 @@
+package com.rkrs.bikethefttracker.validation.annotation;
+
+import com.rkrs.bikethefttracker.validation.validator.ImageValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ImageValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Image {
+
+    String message() default "File is not a valid image.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String[] allowedTypes() default {
+        "image/jpeg",
+        "image/png",
+        "image/webp",
+        "image/heic"
+    };
+
+    long maxSize() default 5 * 1024 * 1024; // 5MB
+}
