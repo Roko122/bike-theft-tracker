69900b0a6e866b9cd82cd5a2a68cae92f498ee0c
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-13 16:32:23 +0200

    BTT-73 Add method to save bike images

--- backend/src/main/java/com/rkrs/bikethefttracker/service/BikeService.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/service/BikeService.java
@@ -1,11 +1,14 @@
 package com.rkrs.bikethefttracker.service;
 
-import com.rkrs.bikethefttracker.entity.Bike;
-import com.rkrs.bikethefttracker.entity.User;
 import com.rkrs.bikethefttracker.dto.CreateBikeRequest;
+import com.rkrs.bikethefttracker.entity.Bike;
+import com.rkrs.bikethefttracker.entity.BikeImage;
+import com.rkrs.bikethefttracker.entity.User;
 import com.rkrs.bikethefttracker.mapper.BikeMapper;
 import com.rkrs.bikethefttracker.repository.BikeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BikeService {
@@ -22,5 +25,17 @@ public class BikeService {
         Bike bikeToCreate = bikeMapper.toBike(bike);
         bikeToCreate.setUser(user);
         return bikeRepository.save(bikeToCreate);
+    }
+
+    public void addImages(Bike bike, List<String> imagePaths) {
+        List<BikeImage> bikeImages = imagePaths.stream().map(imagePath ->
+                BikeImage.builder()
+                        .bike(bike)
+                        .imageName(imagePath)
+                        .build()
+        ).toList();
+
+        bike.getImages().addAll(bikeImages);
+        bikeRepository.save(bike);
     }
 }
