e77dcdde8c3c2c9459fb1e51e324267bae83ce56
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-17 08:41:51 +0200

    BTT-144 Fix typo

--- backend/src/main/java/com/rkrs/bikethefttracker/exception/UserAlreadyExistsException.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/exception/UserAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.rkrs.bikethefttracker.exception;
 
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException(String username) {
-        super("User with username with " + username + " already exists.");
+        super("User with username '" + username + "' already exists.");
     }
 }
