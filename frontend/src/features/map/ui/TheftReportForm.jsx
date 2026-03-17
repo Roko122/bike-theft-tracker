d3db742cf33e59c39240dbf5b54a50fcc779164d
Pearl <helmi.rauhala1@gmail.com>
2026-03-17 11:23:04 +0100

    BTT-75 varas ilmoitusken korjaus kai ok nyt

--- frontend/src/features/map/ui/TheftReportForm.jsx
+++ frontend/src/features/map/ui/TheftReportForm.jsx
@@ -73,10 +73,6 @@ export default function TheftReportForm({
   const [serialNumber, setSerialNumber] = useState('');
   const [bikeDescription, setBikeDescription] = useState('');
 
-  // Ilmoittajan tiedot (bike.user)
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-
   // UI-tilat
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
@@ -211,8 +207,8 @@ export default function TheftReportForm({
         type,
         color,
         serialNumber,
-        description: bikeDescription,
-      },
+        description: bikeDescription
+      }
     };
 
     try {
@@ -535,27 +531,6 @@ export default function TheftReportForm({
           </Form.Group>
 
           <hr />
-
-          {/* User */}
-          <h6>Ilmoittajan tiedot</h6>
-
-          <Form.Group className="mb-3">
-            <Form.Label>Käyttäjänimi</Form.Label>
-            <Form.Control
-              placeholder="tämä kenttä tullaan poistamaan kirjautumisen myötä"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Label>Sähköposti</Form.Label>
-            <Form.Control
-              placeholder="tämä kenttä tullaan poistamaan kirjautumisen myötä"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
 
           <Button
             type="submit"
