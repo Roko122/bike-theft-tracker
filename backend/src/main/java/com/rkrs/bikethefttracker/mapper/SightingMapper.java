13223ed9f90698f73cf91dd7f1d93f8ee419b40c
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-18 13:31:22 +0200

    BTT-153 Add SightingMapper

---
+++ backend/src/main/java/com/rkrs/bikethefttracker/mapper/SightingMapper.java
@@ -0,0 +1,43 @@
+package com.rkrs.bikethefttracker.mapper;
+
+import com.rkrs.bikethefttracker.dto.GeoPoint;
+import com.rkrs.bikethefttracker.dto.SightingRequest;
+import com.rkrs.bikethefttracker.dto.SightingResponse;
+import com.rkrs.bikethefttracker.entity.Sighting;
+import com.rkrs.bikethefttracker.entity.TheftReport;
+import com.rkrs.bikethefttracker.entity.User;
+import org.locationtech.jts.geom.Point;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SightingMapper {
+
+    private final GeoPointMapper geoPointMapper;
+
+    public SightingMapper(GeoPointMapper geoPointMapper) {
+        this.geoPointMapper = geoPointMapper;
+    }
+
+    public Sighting toSighting(SightingRequest sightingRequest, User user, TheftReport theftReport) {
+        Point location = geoPointMapper.toPoint(sightingRequest.location());
+
+        return Sighting.builder()
+                .location(location)
+                .reporter(user)
+                .description(sightingRequest.description())
+                .theftReport(theftReport)
+                .build();
+    }
+
+    public SightingResponse toSightingResponse(Sighting sighting) {
+        GeoPoint geoPoint = geoPointMapper.toGeoPoint(sighting.getLocation());
+
+        return new SightingResponse(
+                sighting.getId(),
+                geoPoint,
+                sighting.getDescription(),
+                sighting.getReporter().getUsername(),
+                sighting.getCreatedAt()
+        );
+    }
+}
