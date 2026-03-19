58b78505e0ac4b89c65e62309f6c6d99d6095583
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-19 08:56:27 +0200

    BTT-132 Implement getSightings and createSighting

--- backend/src/main/java/com/rkrs/bikethefttracker/controller/SightingController.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/controller/SightingController.java
@@ -3,6 +3,7 @@ package com.rkrs.bikethefttracker.controller;
 import com.rkrs.bikethefttracker.dto.SightingRequest;
 import com.rkrs.bikethefttracker.dto.SightingResponse;
 import com.rkrs.bikethefttracker.security.CustomUserDetails;
+import com.rkrs.bikethefttracker.service.SightingService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,15 +14,22 @@ import java.util.UUID;
 @RequestMapping("/api/v1/theft-reports/{theftReportId}/sightings")
 public class SightingController {
 
+    private final SightingService sightingService;
+
+    public SightingController(SightingService sightingService) {
+        this.sightingService = sightingService;
+    }
+
     @GetMapping
     public List<SightingResponse> getSightings(@PathVariable UUID theftReportId) {
-        return null;
+        return sightingService.getAllSightings(theftReportId);
     }
 
     @PostMapping
     public SightingResponse createSighting(@RequestBody SightingRequest sightingRequest,
                                            @PathVariable UUID theftReportId,
                                            @AuthenticationPrincipal CustomUserDetails userDetails) {
-        return null;
+
+        return sightingService.createSighting(sightingRequest, userDetails.getUserEntity(), theftReportId);
     }
 }
