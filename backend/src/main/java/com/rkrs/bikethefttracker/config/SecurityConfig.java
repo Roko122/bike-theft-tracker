b7a76bdc98777c1bf605de9e34c75cb5aa074f1a
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-19 12:57:08 +0200

    BTT-133 Update WebConfig and SecurityConfig to include new path /images/sightings

--- backend/src/main/java/com/rkrs/bikethefttracker/config/SecurityConfig.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/auth/me").authenticated()
                         .requestMatchers(HttpMethod.GET, "/images/bikes/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/images/sightings/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/theft-reports/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/theft-reports").authenticated()
                         .requestMatchers(HttpMethod.GET, "/api/v1/theft-reports/{theftReportId}/sightings").permitAll()
