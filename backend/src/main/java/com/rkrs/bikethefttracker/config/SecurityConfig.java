de1b2a28e2737486b6cf26eaf070778604eeb9c0
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-19 08:56:53 +0200

    BTT-132 Update securityFilterChain to include new endpoints

--- backend/src/main/java/com/rkrs/bikethefttracker/config/SecurityConfig.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/config/SecurityConfig.java
@@ -39,6 +39,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/images/bikes/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/theft-reports/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/theft-reports").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/theft-reports/{theftReportId}/sightings").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/theft-reports/{theftReportId}/sightings").authenticated()
                         .requestMatchers(HttpMethod.GET,"/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.GET,"/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
