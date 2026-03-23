e25e1f9aaa7ab8ca9bdfe3f228fdeb9f258eb156
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-23 15:53:09 +0200

    BTT-135 Add Builder annotation to enable builder

--- backend/src/main/java/com/rkrs/bikethefttracker/entity/Notification.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/entity/Notification.java
@@ -1,10 +1,7 @@
 package com.rkrs.bikethefttracker.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,7 +9,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "notifications")
 @Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor @AllArgsConstructor @Builder
 public class Notification {
 
     @Id
