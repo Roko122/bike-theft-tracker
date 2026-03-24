dd8d6356a108eaaa1203fe5f3750ea4b564a5e9c
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-24 10:04:16 +0200

    BTT-135 Add methods to create, markAsRead, fetch unread and unread count

--- backend/src/main/java/com/rkrs/bikethefttracker/service/NotificationService.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/service/NotificationService.java
@@ -1,8 +1,57 @@
 package com.rkrs.bikethefttracker.service;
 
+import com.rkrs.bikethefttracker.dto.NotificationResponse;
+import com.rkrs.bikethefttracker.entity.Notification;
+import com.rkrs.bikethefttracker.entity.NotificationType;
+import com.rkrs.bikethefttracker.entity.TheftReport;
+import com.rkrs.bikethefttracker.entity.User;
+import com.rkrs.bikethefttracker.exception.NotFoundException;
+import com.rkrs.bikethefttracker.mapper.NotificationMapper;
+import com.rkrs.bikethefttracker.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final NotificationMapper notificationMapper;
+
+    public NotificationService(NotificationRepository notificationRepository, NotificationMapper notificationMapper) {
+        this.notificationRepository = notificationRepository;
+        this.notificationMapper = notificationMapper;
+    }
+
+    public void createNotification(NotificationType type, String message, User recipient, TheftReport theftReport) {
+        Notification notificationToCreate = Notification.builder()
+                .type(type)
+                .message(message)
+                .recipient(recipient)
+                .theftReport(theftReport)
+                .build();
+
+        notificationRepository.save(notificationToCreate);
+    }
+
+    public void markAsRead(User user, UUID notificationId) {
+        Notification notification = notificationRepository.findByIdAndRecipient(notificationId, user)
+                .orElseThrow(() -> new NotFoundException("Notification with id " + notificationId + " not found")
+        );
+
+        notification.setRead(true);
+        notificationRepository.save(notification);
+    }
+
+    public List<NotificationResponse> getUnreadNotifications(User user) {
+        List<Notification> unreadNotifications = notificationRepository.findByRecipientAndReadFalse(user);
+        return unreadNotifications.stream().map(notificationMapper::toNotificationResponse).toList();
+    }
+
+    public int getUnreadNotificationCount(User user) {
+        return notificationRepository.countByRecipientAndReadFalse(user);
+    }
+
 
 }
