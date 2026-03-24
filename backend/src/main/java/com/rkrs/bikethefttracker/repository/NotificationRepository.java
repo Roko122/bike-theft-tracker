111a2856b3b26a0d50179da589cef6a59fbd15da
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-24 10:03:35 +0200

    BTT-135 Add needed methods to handle Notifications

--- backend/src/main/java/com/rkrs/bikethefttracker/repository/NotificationRepository.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/repository/NotificationRepository.java
@@ -1,11 +1,19 @@
 package com.rkrs.bikethefttracker.repository;
 
 import com.rkrs.bikethefttracker.entity.Notification;
+import com.rkrs.bikethefttracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    Optional<Notification> findByIdAndRecipient(UUID notificationId, User recipient);
+
+    List<Notification> findByRecipientAndReadFalse(User user);
+
+    int countByRecipientAndReadFalse(User user);
 }
