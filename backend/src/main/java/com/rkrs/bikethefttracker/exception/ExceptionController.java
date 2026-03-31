28326028ba1874d8aa197470ef53e1fcd07c7a7d
rolfk <172577720+Roko122@users.noreply.github.com>
2026-03-31 15:46:56 +0300

    BTT-159 Add DataIntegrityViolationException handling

--- backend/src/main/java/com/rkrs/bikethefttracker/exception/ExceptionController.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/exception/ExceptionController.java
@@ -2,6 +2,7 @@ package com.rkrs.bikethefttracker.exception;
 
 import com.rkrs.bikethefttracker.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -187,6 +188,19 @@ public class ExceptionController {
                 httpStatus.getReasonPhrase(),
                 httpStatus.value(),
                 ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        HttpStatus httpStatus = HttpStatus.CONFLICT;
+        log.warn(ex.getMessage());
+
+        return new ErrorResponse(
+                httpStatus.getReasonPhrase(),
+                httpStatus.value(),
+                "Request could not be completed due to a data conflict"
         );
     }
 }
