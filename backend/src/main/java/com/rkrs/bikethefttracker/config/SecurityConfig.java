70202d9cc9f1d1034bfa0682a5a3a16bca74b316
rolfk <172577720+Roko122@users.noreply.github.com>
2026-04-02 12:31:57 +0300

    BTT-159 Update SecurityFilterChain to require authentication when fetching sightings

--- backend/src/main/java/com/rkrs/bikethefttracker/config/SecurityConfig.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/images/sightings/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/theft-reports/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/theft-reports").authenticated()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/theft-reports/{theftReportId}/sightings").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/theft-reports/{theftReportId}/sightings").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/v1/theft-reports/{theftReportId}/sightings").authenticated()
                         .requestMatchers(HttpMethod.GET, "/api/v1/notifications/**").authenticated()
                         .requestMatchers(HttpMethod.PATCH, "/api/v1/notifications/{notificationId}/read").authenticated()
