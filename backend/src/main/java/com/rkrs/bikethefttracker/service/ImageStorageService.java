8b58f03115d8b4592725a4cf0ffc24615cda0518
rolfk <172577720+Roko122@users.noreply.github.com>
2026-04-10 13:41:51 +0300

    BTT-140 Add method to delete all images associated with given TheftReport

--- backend/src/main/java/com/rkrs/bikethefttracker/service/ImageStorageService.java
+++ backend/src/main/java/com/rkrs/bikethefttracker/service/ImageStorageService.java
@@ -4,6 +4,7 @@ import com.rkrs.bikethefttracker.exception.InvalidImageException;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
@@ -37,6 +38,11 @@ public class ImageStorageService {
         File folder = this.createFolder(folderName, id);
 
         return handleImages(images, folder);
+    }
+
+    public boolean deleteImages(UUID id) {
+        File file = new File(PROJECT_ROOT + "/images/theft-reports/" + id);
+        return FileSystemUtils.deleteRecursively(file);
     }
 
     private List<String> handleImages(List<MultipartFile> images, File folder) {
