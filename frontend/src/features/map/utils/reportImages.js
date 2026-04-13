ce3bda5c386111b57fc7210fbc50eb76cb2ddf36
Pearl <helmi.rauhala1@gmail.com>
2026-04-13 20:04:00 +0200

    BTT-173 kuvat

--- frontend/src/features/map/utils/reportImages.js
+++ frontend/src/features/map/utils/reportImages.js
@@ -23,5 +23,5 @@ function readImageUrl(item) {
   return '';
 }
 export function getReportImageUrls(images) {
-  return Array.isArray(images) ? images.filter(Boolean) : [];
+  return Array.isArray(images) ? images : [];
 }
